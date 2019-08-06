1c6adbf1b491ff47b9411d06617b1ea3b4f1e576
denys <denys@codefresh.io>
2019-08-06 17:11:02 +0300

    test run

--- runner-entrypoint.sh
+++ runner-entrypoint.sh
@@ -5,11 +5,9 @@ echo ------------------
 #codefresh auth create-context mycontext --api-key $CF_API_KEY
 #codefresh auth use-contex mycontext
 
-cd $GITHUB_EVENT_PATH
-ls
-echo cat event.json
+#cd $GITHUB_EVENT_PATH
+cat /github/workspace/event.json
 
 #codefresh run $PIPELINE_NAME --trigger=$TRIGGER_NAME --sha=$GITHUB_SHA --branch=$BRANCH
 
 echo =======================
-
