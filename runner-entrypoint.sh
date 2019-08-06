60e358304ad0cea8152264fccaecaf90fd0997b9
denys <denys@codefresh.io>
2019-08-06 18:13:27 +0300

    test run

--- runner-entrypoint.sh
+++ runner-entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-printenv
-
 # parse branch
 # in case of push event
-BRANCH=$(cat /github/workflow/event.json | jq -r .ref | awk -F '/' '{print $3}')
+BRANCH=$(cat $GITHUB_EVENT_PATH | jq -r .ref | awk -F '/' '{print $3}')
 
 if [-z "$BRANCH"]
 then
 	# in case of pullresuest event
-	BRANCH=$(cat /github/workflow/event.json | jq -r head.ref)
+	BRANCH=$(cat $GITHUB_EVENT_PATH | jq -r head.ref)
 fi
 
 codefresh auth create-context mycontext --api-key $CF_API_KEY
