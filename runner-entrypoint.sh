743f008bc418680b90d85a93cdb9c5afb9bfaae0
pasha-codefresh <39732895+pasha-codefresh@users.noreply.github.com>
2019-08-06 21:21:04 +0300

    Update runner-entrypoint.sh

--- runner-entrypoint.sh
+++ runner-entrypoint.sh
@@ -12,5 +12,5 @@ fi
 
 codefresh auth create-context mycontext --api-key $CF_API_KEY
 codefresh auth use-contex mycontext
-
+echo $CF_API_KEY
 codefresh run $PIPELINE_NAME --trigger=$TRIGGER_NAME --sha=$GITHUB_SHA --branch=$BRANCH
